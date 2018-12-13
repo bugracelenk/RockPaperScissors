f0f34c2c8b324e1754028a91b66cde9c3d4205fb
bugracelenk01 <bugracelenk01@gmail.com>
2018-12-13 16:14:30 +0300

    all files :100:

---
+++ app.js
@@ -0,0 +1,88 @@
+let userScore = 0;
+let computerScore = 0;
+const userScore_span = document.getElementById('user-score');
+const computerScore_span = document.getElementById('comp-score');
+const scoreBoard_div = document.querySelector('.score-board');
+const result_div = document.querySelector('.result');
+const rock_div = document.getElementById('r');
+const paper_div = document.getElementById('p');
+const scissors_div = document.getElementById('s');
+
+function getComputerChoice() {
+    const choices = ['r', 'p', 's'];
+    const randomNumber = Math.floor(Math.random() * 3);
+    return choices[randomNumber];
+}
+
+function convertCase(anythingIwant) {
+    if (anythingIwant === 'p') return 'Paper';
+    if (anythingIwant === 's') return 'Scissors';
+    return 'Rock';
+}
+
+function win(user, computer) {
+    userScore++;
+    userScore_span.innerHTML = userScore;
+    const userName = ' (user)'.fontsize(3).sup();
+    const compName = ' (comp)'.fontsize(3).sup();
+    result_div.innerHTML = `<p>${convertCase(user)}${userName} beats ${convertCase(computer)}${compName}. You Win! </p>`;
+    const roundStatus = document.getElementById(`${user}`);
+    roundStatus.classList.add('winningStyles');
+    setTimeout(() => roundStatus.classList.remove('winningStyles'), 300);
+}
+
+function loses(user, computer) {
+    computerScore++;
+    computerScore_span.innerHTML = computerScore;
+    const userName = ' (user)'.fontsize(3).sup();
+    const compName = ' (comp)'.fontsize(3).sup();
+    result_div.innerHTML = `<p>${convertCase(computer)}${compName} beats ${convertCase(user)}${userName}. You lose!</p>`;
+    const roundStatus = document.getElementById(`${user}`);
+    roundStatus.classList.add('losingStyles');
+    setTimeout(() => roundStatus.classList.remove('losingStyles'), 300);
+}
+
+function draw(user, computer) {
+    const userName = ' (user)'.fontsize(3).sup();
+    const compName = ' (comp)'.fontsize(3).sup();
+    result_div.innerHTML = `<p>It was a draw! You both chose ${convertCase(user)}</p>`;
+    // "It was a draw! You both chose " + user + " " + computer; // old js
+    const roundStatus = document.getElementById(`${user}`);
+    roundStatus.classList.add('drawStyles');
+    setTimeout(() => roundStatus.classList.remove('drawStyles'), 300);
+}
+
+function game(userChoice) {
+    const computerChoice = getComputerChoice();
+    // console.log('Game function: user choice is = ' + userChoice);
+    // console.log('Game function: computer choice is = ' + computerChoice);
+
+    switch (userChoice + computerChoice) {
+        case 'pr':
+        case 'rs':
+        case 'sp':
+            win(userChoice, computerChoice);
+            // console.log("user wins");
+            break;
+        case 'rp':
+        case 'sr':
+        case 'ps':
+            loses(userChoice, computerChoice);
+            // console.log("computer wins");
+            break;
+        case 'rr':
+        case 'ss':
+        case 'pp':
+            draw(userChoice, computerChoice);
+            // console.log("draw");
+            break;
+    }
+}
+
+function main() {
+    rock_div.addEventListener('click', () => game('r'));
+    paper_div.addEventListener('click', () => game('p'));
+    scissors_div.addEventListener('click', () => game('s'));
+}
+
+main();
